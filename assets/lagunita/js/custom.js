84efad872f06a911ecb215466271aa2d4c4df8c6
A. Karl Kornel <akkornel@stanford.edu>
2018-04-06 11:50:37 -0700

    Import lagunita_jekyll_201804061

--- assets/lagunita/js/custom.js
+++ assets/lagunita/js/custom.js
@@ -1,12 +1,16 @@
-(function ($) {
+// Custom Javascript for Lagunita HTML Theme
+// You can add custom JS functions to this file.
+jQuery(document).ready(function($){
+  // Add "active" to nav element if matches body class of "subnav-n"
+  var bodyClass="";
+  var matches = document.body.className.match(/(^|\s+)(subnav-\d+)(\s|$)/);
+  if (matches) {
+    // found the bodyClass
+    bodyClass = matches[2];
+  }
 
-$('document').ready(function(){
-  $('.sidebar-nav ul').addClass('nav');
-  $('.sidebar-nav li').each(function(i, el) {
-    var re = /(.*)(<a [^>]+>)(.*)(<\/a>)(.*)/; // $1 is before <a>, $3 is inside <a></a>, $5 is after </a>
-    $(el).html( $(el).html().trim().replace(re, '$2$1 $3 $5$4') );
-  });
-  $('.sidebar-nav li.current-cat a, .sidebar-nav li.current_page_item a, .sidebar-nav li.current-menu-item a').addClass('toc_current');
+  if (bodyClass.length > 0){
+    var target = $( '#' + bodyClass );
+    target.addClass('active');
+  }
 });
-
-})(jQuery);
